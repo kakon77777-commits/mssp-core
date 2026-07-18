afa21508312dfd519ebadd5c45bbe2c53d09302c
許筌崴(Neo.K) <kakon77777@gmail.com>
2026-07-18 17:10:01 +0800

    feat(router): export evaluator API

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export * from "./promotion.js";
 export * from "./drift.js";
 export * from "./impact.js";
 export * from "./visualization.js";
+export * from "./router.js";
 export * from "./io.js";
 export * from "./validate.js";
 export * from "./graph.js";
