21d6e530558757f6652f2ab063332c24bf5f3c7d
許筌崴(Neo.K) <kakon77777@gmail.com>
2026-07-18 12:22:24 +0800

    Export architecture drift APIs

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./scanner.js";
 export * from "./classification.js";
 export * from "./classification-report.js";
 export * from "./promotion.js";
+export * from "./drift.js";
 export * from "./io.js";
 export * from "./validate.js";
 export * from "./graph.js";
